9d8b89568260ad68a3c41782165a08fc7332ebbf
MinJeong-Jang <zena4649@gmail.com>
2023-06-08 15:22:52 +0900

    [ADD] Community, Reply

--- mugpet/src/main/java/com/spring/mugpet/dao/mybatis/mapper/CommunityMapper.java
+++ mugpet/src/main/java/com/spring/mugpet/dao/mybatis/mapper/CommunityMapper.java
@@ -3,11 +3,13 @@ package com.spring.mugpet.dao.mybatis.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.spring.mugpet.controller.community.NewCommunityCommand;
 import com.spring.mugpet.domain.Community;
 
 public interface CommunityMapper {
+	
 	public List<Community> getComList(); /*커뮤니티 전체 글 목록*/
 	
 	public List<Community> getMemberComList(int u_id); /*member가 쓴 글 목록*/
@@ -21,4 +23,6 @@ public interface CommunityMapper {
 	public void deleteCom(int com_id); /*커뮤니티 글 삭제*/
 
 	public void updateReplyCnt(@Param("com_id") int com_id, @Param("amount") int amount);
+	
+	public int getU_IdByCommunity(int com_id);
 }
